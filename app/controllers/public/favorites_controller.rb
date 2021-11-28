2c92ea2f1632ac1ecec0ba3a2b41a79fc2e648a0
Tateishi Manaka <monakanomannak.721@gmail.com>
2021-11-28 02:52:07 +0000

    favorite-Ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    shop = Shop.find(params[:shop_id])
-    favorite = current_customer.favorites.new(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    favorite = current_customer.favorites.new(shop_id: @shop.id)
     favorite.save
-    redirect_to shop_path(shop.id)
+    # redirect_to shop_path(shop.id)
   end
 
   def destroy
-    shop = Shop.find(params[:shop_id])
-    favorite = current_customer.favorites.find_by(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    favorite = current_customer.favorites.find_by(shop_id: @shop.id)
     favorite.destroy
-    redirect_to shop_path(shop.id)
+    # redirect_to shop_path(shop.id)
   end
 
 end
