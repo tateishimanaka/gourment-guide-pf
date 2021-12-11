3ffc838ba29ec669ab92a97eba20a3ba936fa95c
ManakaTateishi <monakanomannaka.721@gmail.com>
2021-12-11 03:02:31 +0000

    restaurant-menu-layauto

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @shops_all = Shop.where(is_seleted: false).order("id DESC")
+    @shops_all = Shop.where(is_deleted: false).order("id DESC")
     @shops = @shops_all.limit(5)
   end
 
