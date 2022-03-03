84363696bf81c88a6164e1d0a286e7458caa4740
ManakaTateishi <monakanomannaka.721@gmail.com>
2022-03-03 13:28:34 +0000

    spec-model-shop-completion

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -2,10 +2,56 @@
 
 require 'rails_helper'
 
-RSpec.describe Shop, "モデルに関するテスト" do
-  describe '実際に保存してみる' do
-    it "有効な投稿内容の場合は保存されるか" do
-      expect(FactoryBot.build(:shop)).to be_valid
+RSpec.describe "Shopモデルに関するテスト", type: :model do
+  describe 'バリデーションのテスト' do
+    subject { shop.valid? }
+
+    let(:shop) { build(:shop) }
+
+    context 'shop_nameカラム' do
+      it '空欄でないこと' do
+        shop.shop_name = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'shop_name_kanaカラム' do
+      it '空欄でないこと' do
+        shop.shop_name_kana = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Menuモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Shop.reflect_on_association(:menus).macro).to eq :has_many
+      end
+    end
+
+    context 'Seatモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Shop.reflect_on_association(:seats).macro).to eq :has_many
+      end
+    end
+
+    context 'Imageモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Shop.reflect_on_association(:images).macro).to eq :has_many
+      end
+    end
+
+    context 'Favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Shop.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+
+    context 'Commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Shop.reflect_on_association(:comments).macro).to eq :has_many
+      end
     end
   end
 end
