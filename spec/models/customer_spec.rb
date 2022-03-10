9fdb8f33073553a455c367accc4eedc7499b96ab
ManakaTateishi <monakanomannaka.721@gmail.com>
2022-03-10 12:42:39 +0000

    spec-model-finish

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -14,11 +14,60 @@ RSpec.describe 'Customerモデルのテスト' do
         is_expected.to eq false
       end
     end
-    
+
     context 'first_nameカラム' do
       it '空欄でないこと' do
         customer.first_name = ''
         is_expected.to eq false
+      end
+    end
+
+    context 'last_name_kanaカラム' do
+      it '空欄でないこと' do
+        customer.last_name_kana = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'first_name_kanaカラム' do
+      it '空欄でないこと' do
+        customer.first_name_kana = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'postal_codeカラム' do
+      it '空欄でないこと' do
+        customer.postal_code = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'addressカラム' do
+      it '空欄でないこと' do
+        customer.address = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'telephone_numberカラム' do
+      it '空欄でないこと' do
+        customer.telephone_number = ''
+        is_expected.to eq false
+      end
+    end
+  end
+  
+  describe 'アソシエーションのテスト' do
+    context 'Favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Customer.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+    
+    context 'Commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Customer.reflect_on_association(:comments).macro).to eq :has_many
       end
     end
   end
