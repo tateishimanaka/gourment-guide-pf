16c36b33ff6b0a4391b595c29b502e404aadb9dd
ManakaTateishi <monakanomannaka.721@gmail.com>
2022-03-30 13:21:56 +0000

    spec-system-ヘッダーのリンク

--- spec/system/01_customer_login_before_spec.rb
+++ spec/system/01_customer_login_before_spec.rb
@@ -17,9 +17,19 @@ describe '[step1]customerログイン前のテスト' do
         expect(page).to have_content 'Gourment Guide'
       end
 
-      it '「お店の情報を公開」のリンクが表示される' do
+      it '「お店の情報を公開」のリンクが表示される: 左から１番目のリンクが「お店の情報を公開する」である' do
         shop_link = find_all('a')[1].native.inner_text
         expect(shop_link).to match "お店の情報を公開"
+      end
+
+      it '「お店を探す」のリンクが表示される: 左から２番目のリンクが「お店を探す」である' do
+        customer_link = find_all('a')[2].native.inner_text
+        expect(customer_link).to match "お店を探す"
+      end
+
+      it '「shop List」のリンクが表示される: 左から３番目のリンクが「shop List」である' do
+        shop_list_link = find_all('a')[3].native.inner_text
+        expect(shop_list_link).to match "shop List"
       end
     end
   end
