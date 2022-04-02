aceea2003a2c474b75ca1bfad1baae30cfabfb17
ManakaTateishi <monakanomannaka.721@gmail.com>
2022-04-02 05:06:10 +0000

    spec-system-リンクの内容

--- spec/system/01_customer_login_before_spec.rb
+++ spec/system/01_customer_login_before_spec.rb
@@ -22,14 +22,29 @@ describe '[step1]customerログイン前のテスト' do
         expect(shop_link).to match "お店の情報を公開"
       end
 
+      it '「お店の情報を公開」のリンクの内容が正しい' do
+        shop_link = find_all('a')[1].native.inner_text
+        expect(page).to have_link shop_link, href: restaurant_about_path
+      end
+
       it '「お店を探す」のリンクが表示される: 左から２番目のリンクが「お店を探す」である' do
         customer_link = find_all('a')[2].native.inner_text
         expect(customer_link).to match "お店を探す"
       end
 
+      it '「お店を探す」のリンクの内容が正しい' do
+        customer_link = find_all('a')[2].native.inner_text
+        expect(page).to have_link customer_link, href: about_path
+      end
+
       it '「shop List」のリンクが表示される: 左から３番目のリンクが「shop List」である' do
         shop_list_link = find_all('a')[3].native.inner_text
         expect(shop_list_link).to match "shop List"
+      end
+
+      it '「shop List」のリンクの内容が正しい' do
+        shop_list_link = find_all('a')[3].native.inner_text
+        expect(page).to have_link shop_list_link, href: shops_path
       end
     end
   end
