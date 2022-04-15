c961c8f9b101231d2b51d6119df90d4c4bdb2f3c
ManakaTateishi <monakanomannaka.721@gmail.com>
2022-04-15 13:02:47 +0000

    spec-system-customer新規登録表示の確認

--- spec/system/01_customer_login_before_spec.rb
+++ spec/system/01_customer_login_before_spec.rb
@@ -368,4 +368,67 @@ describe '[step1]customerログイン前のテスト' do
       end
     end
   end
+
+  describe '一般会員新規登録のテスト' do
+    before do
+      visit new_customer_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/customer/sign_up'
+      end
+      it '「新規会員登録」と表示される' do
+        expect(page).to have_content '新規会員登録'
+      end
+      it 'last_nameフォームが表示される' do
+        expect(page).to have_field 'customer[last_name]'
+      end
+      it 'first_nameフォームが表示される' do
+        expect(page).to have_field 'customer[first_name]'
+      end
+      it 'last_name_kanaフォームが表示される' do
+        expect(page).to have_field 'customer[last_name_kana]'
+      end
+      it 'first_name_kanaフォームが表示される' do
+        expect(page).to have_field 'customer[first_name_kana]'
+      end
+      it 'postal_codeフォームが表示される' do
+        expect(page).to have_field 'customer[postal_code]'
+      end
+      it 'addressフォームが表示される' do
+        expect(page).to have_field 'customer[address]'
+      end
+      it 'telephone_numberフォームが表示される' do
+        expect(page).to have_field 'customer[telephone_number]'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'customer[email]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'customer[password]'
+      end
+      it 'password_confirmationフォームが表示される' do
+        expect(page).to have_field 'customer[password_confirmation]'
+      end
+      it '「登録」ボタンが表示される' do
+        expect(page).to have_button '登録'
+      end
+    end
+
+    context '新規登録成功のテスト' do
+      before do
+        fill_in 'customer[last_name]', with: Faker::Lorem.characters(number: 2)
+        fill_in 'customer[first_name]', with: Faker::Lorem.characters(number: 2)
+        fill_in 'customer[last_name_kana]', with: Faker::Lorem.characters(number: 5)
+        fill_in 'customer[first_name_kana]', with: Faker::Lorem.characters(number: 3)
+        fill_in 'customer[postal_code]', with: Faker::Lorem.characters(number: 7)
+        fill_in 'customer[address]', with: Faker::Lorem.characters(number: 7)
+        fill_in 'customer[telephone_number]', with: Faker::Lorem.characters(number: 10)
+        fill_in 'customer[password]', with: 'password'
+        fill_in 'customer[password_confirmation]', with: 'password'
+        fill_in 'customer[email]', with: Faker::Internet.email
+      end
+    end
+  end
 end
